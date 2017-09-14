2e0a702f815e1a1db12177e0ea618e0891cb6cf4
feche <fecheromero@gmail.com>
2017-09-14 15:56:10 -0300

    arreglando el disabled en el editor de conclusion

--- app/components/editor-de-conclusion.js
+++ app/components/editor-de-conclusion.js
@@ -11,10 +11,10 @@ export default Ember.Component.extend({
     if(this.get('temaDeMinuta.actionItems').some((actionItem)=>{
         return  !actionItem.descripcion || actionItem.responsables.length<=0;
       })){
-      return "disabled";
+      return true;
     }
     else{
-      return "";
+      return false;
     }
   }),
   actions:{
