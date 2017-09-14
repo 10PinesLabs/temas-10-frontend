c9b0334ff89e928eff89f7293d233a418013f790
feche <fecheromero@gmail.com>
2017-09-14 16:56:56 -0300

    retoques en minutos-de-duracion

--- app/helpers/minutos-de-duracion.js
+++ app/helpers/minutos-de-duracion.js
@@ -2,11 +2,12 @@ import Ember from 'ember';
 
 export function minutosDeDuracion(params/*, hash*/) {
 
-  var nombre=params[0];
-  var duraciones=params[1];
-  return '('+ duraciones.find((duracion)=>{
+  let nombre=params[0];
+  let duraciones=params[1];
+  let cantidadDeMinutos=duraciones.find((duracion)=>{
     return duracion.nombre===nombre;
-  }).cantidadDeMinutos + ' minutos)';
+  }).cantidadDeMinutos;
+  return '('+ cantidadDeMinutos + ' minutos)';
 }
 
 export default Ember.Helper.helper(minutosDeDuracion);
